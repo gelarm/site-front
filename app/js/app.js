ebbcc28570d4c2b937bcd11190beb732ba613f28
Дмитрий Десятников <sickmeppo@gmail.com>
2020-08-12 16:36:25 +0300

    add new sideout menu

--- app/js/app.js
+++ app/js/app.js
@@ -1,10 +1,15 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
 
-	var btnToggle = document.querySelector('.wrap-nav__toggle');
-	btnToggle ? btnToggle.addEventListener('click', function (e) {
+  var btnToggle = document.querySelector('.wrap-nav__toggle');
+  var navList = document.querySelector('.wrap-nav__list')
+  // var container = document.querySelector('.js-container')
+
+  btnToggle ? btnToggle.addEventListener('click', function (e) {
     e.preventDefault();
-    btnToggle.classList.toggle('wrap-nav__toggle-active')
-    console.log(btnToggle)
+    btnToggle.classList.toggle('wrap-nav__toggle-active') ?
+      navList.classList.add('wrap-nav__list-active') :
+      navList.classList.remove('wrap-nav__list-active')
+
   }) : btnToggle
 
 });
